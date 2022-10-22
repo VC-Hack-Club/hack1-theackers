b0a406944417141c13df956e799e8dba08e846f6
janmashtami <111668865+janmashtami@users.noreply.github.com>
2022-10-22 13:01:49 -0700

    Dashboard update

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Dashboard from './views/Dashboard';
+// import App from './App';
+import Dashboard from './views/Dashboard.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
